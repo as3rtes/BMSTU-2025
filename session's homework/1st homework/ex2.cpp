132c9aec56ed9f5442329a5ef5651d277a8ddb0e
as3 <z-official@bk.ru>
2025-09-24 10:55:52 +0300

    Create ex2.cpp

---
+++ session's homework/1st homework/ex2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a,b;
+	cout << "Enter length: ";
+	cin >> a;
+	cout << "Enter width: ";
+	cin >> b;
+	cout << "Rectangle area: " << a * b;
+
+
+
+	return 0;
+}

caa6377cff3457d04968f422d47b0d4969e9abf8
as3 <z-official@bk.ru>
2025-09-24 11:31:52 +0300

    Create ex7.cpp

---
+++ session's homework/1st homework/ex7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cout << "Enter n: ";
+	cin >> num;
+	for (int i = 1; i < num+1; i++) {
+		cout <<i << " - " << i * i << endl;
+	
+	}
+	return 0;
+}
+	

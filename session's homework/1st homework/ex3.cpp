0b271b1ac0b0fbefce97f03f272e739b63ebb341
as3 <z-official@bk.ru>
2025-09-24 11:03:37 +0300

    Create ex3.cpp

---
+++ session's homework/1st homework/ex3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double Celsius;
+	cout << "Enter degrees: ";
+	cin >> Celsius;
+	cout << "The Faringate: " << Celsius * 1.4 + 32 ;
+
+
+	return 0;
+}

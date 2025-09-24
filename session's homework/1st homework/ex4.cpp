0e13f8342a70461aa0b41d1a67641e5640e8aee0
as3 <z-official@bk.ru>
2025-09-24 11:11:48 +0300

    Create ex4.cpp

---
+++ session's homework/1st homework/ex4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num;
+	cout << "Enter num: ";
+	cin >> num;
+	if (num % 2 == 0) {
+		cout << "it is even number";
+	}
+	else {
+		cout << "it is odd number: ";
+	}
+	return 0;
+}

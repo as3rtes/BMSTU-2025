83f6493d77b30b92f0a1fdbad1449ed472da968a
as3 <z-official@bk.ru>
2025-10-05 03:34:47 +0300

    Create ex1.cpp

---
+++ session's homework/2.1st OAiP homework geometrical/ex1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double Xcord[3], Ycord[3];
+
+    for (int i = 0; i < 3; i++) {
+        cout << "x" << i + 1 << ':';
+        cin >> Xcord[i];
+
+        cout << "y" << i + 1 << ':';
+        cin >> Ycord[i];
+    }
+    for (int i = 0; i < 3; i++) {
+        cout << Xcord[i] << endl;
+        cout << Ycord[i] << endl;
+    }
+    double Square = 0.5 * abs(Xcord[0] * (Ycord[1] - Ycord[2]) + Xcord[1] * (Ycord[2] - Ycord[0]) + Xcord[2] * (Ycord[0] - Ycord[1]));
+    cout << "square is: " << Square;
+
+    return 0;
+}
